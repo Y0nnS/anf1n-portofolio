fb5c6c2e9515b0c80451bc6da334253ff1d6974d
Y0nnS <112255500+Y0nnS@users.noreply.github.com>
2025-01-15 23:45:07 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("burger-menu").addEventListener("click", function () {
+    const mobileNav = document.getElementById("mobile-nav");
+    mobileNav.classList.toggle("hidden"); // Toggle the visibility of the mobile nav
+});
