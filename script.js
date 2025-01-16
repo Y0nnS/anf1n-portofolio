452fececffc5c30c4e1ccf7722c16db86a0b0d40
Y0nnS <112255500+Y0nnS@users.noreply.github.com>
2025-01-16 07:29:07 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ document.getElementById("burger-menu").addEventListener("click", function () {
     const mobileNav = document.getElementById("mobile-nav");
     mobileNav.classList.toggle("hidden"); // Toggle the visibility of the mobile nav
 });
+
+// Event listener untuk card
+const cards = document.querySelectorAll(".card");
+const modal = document.getElementById("imageModal");
+const modalImage = document.getElementById("modalImage");
+
+cards.forEach(card => {
+    card.addEventListener("click", () => {
+        const imageSrc = card.querySelector("img").src;
+        modalImage.src = imageSrc;
+        modal.classList.remove("hidden");
+    });
+});
+
+function closeModal() {
+    modal.classList.add("hidden");
+}
